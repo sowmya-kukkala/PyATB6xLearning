649ed7b8bd146d057bfbeedd77b6deac6f5082dc
Sowmya-Kukkala <sowmya.kukkala@gmail.com>
2025-11-19 22:45:39 +0530

    06th Nov 2025 - Pushed Code and Added Task completed files

--- task/Task011_06thNov_01.py
+++ task/Task011_06thNov_01.py
@@ -1,6 +1,17 @@
 # Right Angled Triangle
 
-for i in range(1, 6):
-    for j in range(1,i+1):
-        print("*", end="")
+# Method 1:
+
+# for i in range(1, 6):
+#     for j in range(1,i+1):
+#         print("*", end="")
+#     print()
+
+# Method 2:
+for i in range(5):
+    for j in range(5):
+        if(i>=j):
+            print("*", end=" ")
+        else:
+            print(end=" ")
     print()
